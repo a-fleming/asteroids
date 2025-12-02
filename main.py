63a12b553a29e06dcec213f5849a6d49a0a4892e
Aaron Fleming <amfleming15@gmail.com>
2025-12-02 06:09:19 -0800

    Add player and movement functionality

--- main.py
+++ main.py
@@ -2,21 +2,26 @@ import sys
 import os
 import pygame
 
-# # show which Python and pygame are being used (useful for virtualenv debugging)
-# print(f"Python executable: {sys.executable}")
-# print(f"In virtualenv: {getattr(sys, 'base_prefix', None) != getattr(sys, 'prefix', None)}")
-# print(f"Pygame module: {getattr(pygame, '__file__', 'built-in or frozen')}")
-
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -24,7 +29,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        
+        for entity in updatable:
+            entity.update(dt)
+        for entity in drawable:
+            entity.draw(screen)
+        
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000.0  # Limit to 60 FPS
+ 
 if __name__ == "__main__":
     main()
